6021d1e276aa2ba63b82b76c064f0b5c97434c82
Yusariii <khailenguyen2004@gmail.com>
2026-03-27 11:37:55 +0700

    Fix annotation

--- src/main/java/com/khai/TaskFlowApi/dto/auth/request/RegisterRequest.java
+++ src/main/java/com/khai/TaskFlowApi/dto/auth/request/RegisterRequest.java
@@ -1,9 +1,8 @@
 package com.khai.TaskFlowApi.dto.auth.request;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class RegisterRequest {
     
@@ -12,13 +11,11 @@ public class RegisterRequest {
     private String email;
 
     @NotBlank
-    @Min(8)
-    @Max(100)
+    @Size(min = 8, max = 100)
     private String password;
 
     @NotBlank
-    @Min(2)
-    @Max(100)
+    @Size(min = 2, max = 100)
     private String fullName;
 
     public String getEmail() {
